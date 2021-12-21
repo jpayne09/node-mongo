23e799752caecf18d72bbd68b06f183a5315a30a
John Payne <johpayne@geico.com>
2021-12-21 15:24:12 -0500

    Node callback hell and promises

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert').strict;
 const dbopper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
 
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-
-    assert.strictEqual(err, null);
+MongoClient.connect(url, { useUnifiedTopology: true }).then(client => {
 
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
 
-    db.dropCollection('campsites', (err, result) => {
-        assert.strictEqual(err, null);
-        console.log('Dropped Collection', result);
+    db.dropCollection('campsites')
+        .then(result => {
+            console.log('Dropped Collection', result);
+        })
+        .catch(err => console.log('No collection to dro['));
 
-        dbopper.insertDocument(db,{name: "Breadcrumb Trail Campground", description: "Test"},
-            'campsites',result => {
+    dbopper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test" }, 'campsites')
+        .then(result => {
             console.log('Insert Document:', result.ops);
-            dbopper.findDocuments(db, 'campsites', docs => {
-                console.log('Found Documents:', docs);
 
-                dbopper.updateDocument(db, {name: "Breadcrumb Trail Campground"}, 
-                {description: "Updated Test Description"}, 'campsites', 
-                result => {
-                    console.log('Updatd Document Count:', result.result.nModified);
+            return dbopper.findDocuments(db, 'campsites');
+        })
+        .then(docs => {
+            console.log('Found Documents:', docs);
 
-                    dbopper.findDocuments(db, 'campsites', docs => {
-                        console.log('Found Documents:', docs);
+            return dbopper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
+                { description: "Updated Test Description" }, 'campsites');
+        })
+        .then(result => {
+            console.log('Updatd Document Count:', result.result.nModified);
 
-                        dbopper.removeDocument(db, {name: "Breadcrumb Trail Campground"},
-                        'campsites', result => {
-                            console.log('Deleted Document Count:', result.deletedCount);
+            return dbopper.findDocuments(db, 'campsites');
+        })
+        .then(docs => {
+            console.log('Found Documents:', docs);
 
-                            client.close()
-                        })
-                    })
-                })
-            }) 
+            return dbopper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
+                'campsites')
+        })
+        .then(result => {
+            console.log('Deleted Document Count:', result.deletedCount);
+
+            client.close();
+        })
+        .catch(err => {
+            console.log(err);
+            client.close();
         });
-    });
-});
+})
+    .catch(err => console.log(err));
