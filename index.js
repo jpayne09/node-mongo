66a3eb346ded108b490fdc058024c8cf9fceb45f
John Payne <johpayne@geico.com>
2021-12-21 14:37:42 -0500

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert').strict;
+const dbopper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
@@ -16,19 +17,29 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
         assert.strictEqual(err, null);
         console.log('Dropped Collection', result);
 
-        const collection = db.collection('campsites');
-
-        collection.insertOne({name: "Breadcrumb Trail Campground", description: "Test"},
-        (err, result) => {
-            assert.strictEqual(err, null);
+        dbopper.insertDocument(db,{name: "Breadcrumb Trail Campground", description: "Test"},
+            'campsites',result => {
             console.log('Insert Document:', result.ops);
-
-            collection.find().toArray((err, docs) => {
-                assert.strictEqual(err, null);
+            dbopper.findDocuments(db, 'campsites', docs => {
                 console.log('Found Documents:', docs);
 
-                client.close();
-            });
+                dbopper.updateDocument(db, {name: "Breadcrumb Trail Campground"}, 
+                {description: "Updated Test Description"}, 'campsites', 
+                result => {
+                    console.log('Updatd Document Count:', result.result.nModified);
+
+                    dbopper.findDocuments(db, 'campsites', docs => {
+                        console.log('Found Documents:', docs);
+
+                        dbopper.removeDocument(db, {name: "Breadcrumb Trail Campground"},
+                        'campsites', result => {
+                            console.log('Deleted Document Count:', result.deletedCount);
+
+                            client.close()
+                        })
+                    })
+                })
+            }) 
         });
     });
 });
